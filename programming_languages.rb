eaa757967ea8e4816c8ca59418581f77c46e6849
Henry Sztanski <henry@sztanski.com>
2018-12-15 14:59:28 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,46 @@
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+
+  }
+}
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  new_style = {style: []}
+  languages.each { |s, l| new_hash.merge!(l) }
+  new_hash.each { |k, v| v.merge!(new_style) }
+
+  languages.each do |s, l|
+    l.each {|k, v| puts k}
+  end
+
+  new_hash
 end
+
+puts reformat_languages(languages)
